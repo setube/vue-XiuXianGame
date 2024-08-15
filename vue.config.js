ee0f6f41810c0d664d721b7df94e3e36b7f6f6a9
谦君 <73606411+setube@users.noreply.github.com>
2024-08-15 18:04:24 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -1,43 +1,40 @@
 const WebpackObfuscator = require('webpack-obfuscator');
-const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
 module.exports = {
     parallel: true,
     publicPath: './',
     productionSourceMap: false,
     chainWebpack: config => {
+        config.module
+            .rule('js')
+            .use('babel-loader')
+            .loader('babel-loader')
+            .tap(options => {
+                options.cacheDirectory = true;
+                return options;
+            });
         config.cache(true);
-        if (process.env.ANALYZE === 'true') {
-            const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-            config.plugin('bundle-analyzer').use(BundleAnalyzerPlugin, [{ analyzerMode: 'static' }]);
-        }
     },
     configureWebpack: {
         plugins: [
-            new WebpackObfuscator({
-                log: false,
-                compact: true,
-                stringArray: true,
-                renameGlobals: false,
-                selfDefending: false,
-                debugProtection: false,
-                rotateStringArray: true,
-                deadCodeInjection: false,
-                stringArrayEncoding: ['none'],
-                disableConsoleOutput: true,
-                stringArrayThreshold: 0.75,
-                controlFlowFlattening: false,
-                unicodeEscapeSequence: false,
-                identifierNamesGenerator: 'hexadecimal'
-            }, []),
-            new CompressionWebpackPlugin({
-                minRatio: 1,
-                test: /\.(html|js|css)$/,
-                filename: '[path].gz[query]',
-                algorithm: 'gzip',
-                threshold: 10240,
-                deleteOriginalAssets: false
-            })
+            ...(process.env.NODE_ENV === 'production' ? [
+                new WebpackObfuscator({
+                    log: false,
+                    compact: true,
+                    stringArray: true,
+                    renameGlobals: false,
+                    selfDefending: false,
+                    debugProtection: false,
+                    rotateStringArray: true,
+                    deadCodeInjection: false,
+                    stringArrayEncoding: ['none'],
+                    disableConsoleOutput: true,
+                    stringArrayThreshold: 0.75,
+                    controlFlowFlattening: false,
+                    unicodeEscapeSequence: true,
+                    identifierNamesGenerator: 'hexadecimal'
+                }, [])
+            ] : [])
         ]
     }
 };

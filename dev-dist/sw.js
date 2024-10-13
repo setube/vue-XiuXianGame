a17a40f91a793163222238086ee14f9a91c58694
wenmoux <wenmoux@gmail.com>
2024-10-13 23:47:52 +0800

    fix:修复猜拳bug

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7sihfs4vov"
+    "revision": "0.qpsqe14bgto"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
